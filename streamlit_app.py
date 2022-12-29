299f2e3db4bcba08645682f59ced757850cf9e89
gridgoogl011 <121600039+gridgoogl011@users.noreply.github.com>
2022-12-29 09:31:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ st.write("[![Star])")
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Claire Deniau, 'Affiliate Marketer')
+st.header('Claire Deniau, 'affiliate marketer')
 
-st.info('Full Time Writter,  I'm a team player who is eager to collaborate and offer my creative input. I'm dedicated to creating stories that will captivate and inspire my readers.')
+st.info('Full Time Writter, I'm a team player who is eager to collaborate and offer my creative input. I'm dedicated to creating stories that will captivate and inspire my readers.')
 
 icon_size = 20
 
